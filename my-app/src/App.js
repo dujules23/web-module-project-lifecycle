5e8641ddd10021f09063d412a0e4811ce189c10c
Durrell Jules <dujules23@gmail.com>
2021-06-04 12:37:45 -0400

    successful api call was made to dujules23

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
+    console.log("App Constructor")
     this.state ={
       user: []
     }
   }
+  //axios call will go here
+  componentDidMount() {
+    console.log("App CDM")
+    axios.get(`https://api.github.com/users/dujules23`)
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err => console.log(err))
+  }
 
+
+  componentDidUpdate() {
+    console.log("App CDU")
+  }
 
   render () {
+    console.log("App Render")
     return (
       <div className="App">
         <h1>Git Hub User</h1>

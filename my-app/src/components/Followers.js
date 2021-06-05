1d45cae993a0c4a98b9e8b8e6d34ad94852407fe
Durrell Jules <dujules23@gmail.com>
2021-06-04 21:10:46 -0400

    completed styling, will add finishing touches

--- my-app/src/components/Followers.js
+++ my-app/src/components/Followers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Followers.css'
 
 import axios from 'axios';
 
--- my-app/src/components/UserCard.js
+++ my-app/src/components/UserCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './UserCard.css'
 
 import Followers from './Followers'
 
